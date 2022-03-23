105ac808090a4d8a3476f6e09483ecb5eee7f7ff
Kooky <kooky1001@gmail.com>
2022-03-23 17:12:17 +0900

    list&detail 컨트롤러 추가

--- src/main/java/edu/web/controller/notice/DetailController.java
+++ src/main/java/edu/web/controller/notice/DetailController.java
@@ -1,4 +1,4 @@
-package edu.web.controller;
+package edu.web.controller.notice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -6,13 +6,11 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
-public class IndexController implements Controller {
+public class DetailController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mv = new ModelAndView(); 
-		mv.addObject("data", "Hello Spring MVC~!");
-		mv.setViewName("index");
+		ModelAndView mv = new ModelAndView();
 		
 		return mv;
 	}
