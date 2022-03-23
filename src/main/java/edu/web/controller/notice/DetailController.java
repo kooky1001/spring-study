e76c451bd48f6c8654ff51c7a1b36206a25bad70
Kooky <kooky1001@gmail.com>
2022-03-23 19:32:37 +0900

    tiles 적용 + wildcard

--- src/main/java/edu/web/controller/notice/DetailController.java
+++ src/main/java/edu/web/controller/notice/DetailController.java
@@ -10,7 +10,7 @@ public class DetailController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("notice.detail");
 		
 		return mv;
 	}
--- src/main/java/edu/web/controller/notice/ListController.java
+++ src/main/java/edu/web/controller/notice/ListController.java
@@ -10,7 +10,7 @@ public class ListController implements Controller {
 
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ModelAndView mv = new ModelAndView();
+		ModelAndView mv = new ModelAndView("notice.list");
 		return mv;
 	}
 
