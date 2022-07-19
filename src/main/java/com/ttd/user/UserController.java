815f0bfb9ab824f1f8725cf66ea85098412e3cd1
Kooky <kooky1001@gmail.com>
2022-07-19 12:13:07 +0900

    user list 추가

--- src/main/java/com/ttd/user/UserController.java
+++ src/main/java/com/ttd/user/UserController.java
@@ -1,25 +1,41 @@
 package com.ttd.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
+	
+	private List<User> list = new ArrayList<User>();
 
-	@GetMapping("/join")
+	@GetMapping("join")
 	public ModelAndView join() {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("join");
+		mv.setViewName("/user/join");
 		return mv;
 	}
 	
-	@PostMapping("/join")
+	@PostMapping("join")
 	public ModelAndView create(User user) {
 		ModelAndView mv = new ModelAndView();
 		System.out.println(user);
-		mv.setViewName("redirect:/");
+		list.add(user);
+		mv.setViewName("redirect:/user/list");
+		return mv;
+	}
+
+	@GetMapping("list")
+	public ModelAndView list() {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("user", list);
+		mv.setViewName("/user/list");
 		return mv;
 	}
 }
