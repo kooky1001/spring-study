e75ba2a0943e81ef1fb4914137a3105260d0b693
Kooky <kooky1001@gmail.com>
2022-08-08 10:15:46 +0900

    jquery없이 리플 쓸 수 있도록 수정중

--- src/main/java/com/ttd/controller/ReplyController.java
+++ src/main/java/com/ttd/controller/ReplyController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -43,7 +44,7 @@ public class ReplyController {
 	
 	@ResponseBody
 	@PostMapping("create")
-	public Reply createApi(@PathVariable long id, String content, HttpSession session) {
+	public Reply createApi(@PathVariable long id, @RequestBody String content, HttpSession session) {
 		Validation validation = validate(session);
 		if (!validation.isValid()) {
 			return null;
