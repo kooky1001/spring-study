6eb599cc0a767e24e7354c9686dea7702f3bc6f3
Kooky <kooky1001@gmail.com>
2024-04-12 09:11:34 +0900

    checkstyle 적용

--- src/main/java/org/example/springjsp/SpringJspApplication.java
+++ src/main/java/org/example/springjsp/SpringJspApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJspApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringJspApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJspApplication.class, args);
+	}
 
 }
